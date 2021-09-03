e2ee558c629f62f0c0377e73f04f37ed5d5116ae
David <david.jelis@gmail.com>
2021-09-02 21:24:01 -0400

    Initial

---
+++ In class 9-2.py
@@ -0,0 +1,37 @@
+
+# firstName = "David"
+# lastName = "Jelis"
+#
+# print("Welcome " + firstName + " " + lastName + "! You are starting to learn Python")
+#
+# string1 = "AABCAAADA"
+# k = 3
+# s1 = string1[:k]
+# s2 = string1[k:k+3]
+#
+# print(s1)
+# print(s2)
+
+
+
+# Problem 1
+str1 = "Welcome Home!"
+
+
+
+# Problem 2
+s1 = "David"
+s2 = "Jelis"
+s3 = s1[0] + s2[0] + s1[len(s1)//2] + s2[len(s2)//2] + s1[-1] + s2[-1]
+
+print(s3)
+
+# Problem 3
+
+
+
+# Problem 4
+
+st1 = "Hello"
+new = st1[::-1].capitalize()
+print(new.capitalize())
