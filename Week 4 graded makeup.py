9a5f475eef1f621b1564cb18c3379f864408d96f
David <david.jelis@gmail.com>
2021-09-20 21:59:31 -0400

    last

--- Week 4 graded makeup.py
+++ Week 4 graded makeup.py
@@ -1,21 +1,16 @@
-
+#create list
 student_list=[['Harry', 37.31], ['Berry', 37.21], ['Tina', 37.2], ['Akriti', 41], ['Harsh', 39], ["Mike", 27], ["Ella", 90]]
 
+#loop to create second list of just scores to sort
 scores =[]
 for names in student_list:
     scores.append(names[1])
 
-
-
+#sort scores min to max
 sorted_score = sorted(scores)
-
 print("Name   Score")
-n=0
 
-
+#loop to print names and scores based on score list using index to find pair 
 for number in sorted_score:
-    #print(number)
     x = scores.index(number)
-    #print(x)
-    #print(i)
     print(student_list[x][0] + "   " + str(student_list[x][1]))
